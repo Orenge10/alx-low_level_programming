7a4a2abaedcd383396672956d15e81c49b814662
Orenge10 <pamiorenge@gmail.com>
2022-07-05 13:11:59 -0700

    initial commit

--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-all work
+c
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * File: main.h
- * Description: header file containing The prototypes of all your functions and the prototype of the function _putchar
- */
-
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
+char _putchar(char c);
 
 
 
+
+#endif /* MAIN_H */
