e1655f0624761b6ac83615a13bc18940aa36d4c7
Orenge10 <pamiorenge@gmail.com>
2022-07-05 05:08:13 -0700

    tasks

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,12 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
+ * _putchar - writes the character c
+ *@c: character to be printed
  * Return: Always 0 (Success)
  */
 
-int main(void)
+int _putchar(char c)
 {
-	char c[] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-
-		_putchar(c[i]);
-	}
-	_putchar('\n');
-
 	return (0);
 }
