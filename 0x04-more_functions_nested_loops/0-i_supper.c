4d9b9a53222e1c21a03164b46d95b0547c92b4f5
Orenge10 <pamiorenge@gmail.com>
2022-07-08 05:01:09 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-i_supper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function that cjecks uppercase character.
+ *
+ * Return: 1 if  c is uppercase, otherwise 0.
+ */
+int main(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
