92792c3d0997593cfb65db518f641f2659f6ad14
Orenge10 <pamiorenge@gmail.com>
2022-07-12 04:25:07 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @n : pointer of n and return value of 98.
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
